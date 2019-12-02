f1638327dff189d4c936911ed39eeebabbeb463a
Alfred Sjöholm <alfsjo0116@skynet.halmstad.se>
2019-12-02 09:41:49 +0100

    Warcraft 3 mod omegalol

---
+++ moba test code/moba test code/Agility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moba_test_code
+{
+    class Agility : BasicStats
+    {
+        double agility;
+
+        double levelAgi;
+        //levelagi = levelAgi * level + agility
+
+        int agilityamplifier;
+
+        /*if ( heroStat == agilityHero){
+            lowdamage + agilityamplifier;
+            maxdamage + agilityamplifier;
+       }*/
+    }
+}
---
+++ moba test code/moba test code/BasicStats.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moba_test_code
+{
+    class BasicStats
+    {
+        enum heroStat {agilityHero, strengthHero, intelligenceHero }
+        int level;
+        double health;
+        double mana;
+        int low_Damage;
+        int max_Damage;
+        //Random damageDealer = new Random();
+        //int Damage = damageDealer.Next(low_Damage, max_damage);
+
+        //int/double movementSpeed;
+
+
+        /*if ( heroStat == agilityHero){
+            lowdamage = lowdamage + agilityamplifier;
+            maxdamage = maxdamage + agilityamplifier;
+       }*/
+       /* if (heroStat == strengthHero){
+          lowdamage = lowdamage + strengthamplifier;
+          maxdamage = maxdamage + strengtamplifier;
+        }*/
+        /* if (heroStat == intelligenceHero){
+          lowdamage = lowdamage + intelligenceamplifier;
+          maxdamage = maxdamage + intelligenceamplifier;
+          }*/
+    }
+}
