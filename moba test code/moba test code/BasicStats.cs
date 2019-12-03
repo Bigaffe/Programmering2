ea46cc0176f85c4048c94b7dac62f263fae89d72
Alfred Sjöholm <alfsjo0116@skynet.halmstad.se>
2019-12-03 14:53:36 +0100

    push  and commit

--- moba test code/moba test code/BasicStats.cs
+++ moba test code/moba test code/BasicStats.cs
@@ -9,28 +9,38 @@ namespace moba_test_code
     class BasicStats
     {
         enum heroStat {agilityHero, strengthHero, intelligenceHero }
+
         int level;
         double health;
         double mana;
+
+        double healthReg;
+        double manaReg;
+
         int low_Damage;
         int max_Damage;
         //Random damageDealer = new Random();
         //int Damage = damageDealer.Next(low_Damage, max_damage);
 
-        //int/double movementSpeed;
+
+        int movementSpeed;
+
+        
 
 
         /*if ( heroStat == agilityHero){
-            lowdamage = lowdamage + agilityamplifier;
-            maxdamage = maxdamage + agilityamplifier;
+            lowdamage = lowdamage + agilityamplifier * level;
+            maxdamage = maxdamage + agilityamplifier * level;
        }*/
+
        /* if (heroStat == strengthHero){
-          lowdamage = lowdamage + strengthamplifier;
-          maxdamage = maxdamage + strengtamplifier;
+          lowdamage = lowdamage + strengthamplifier * level;
+          maxdamage = maxdamage + strengtamplifier * level;
         }*/
+
         /* if (heroStat == intelligenceHero){
-          lowdamage = lowdamage + intelligenceamplifier;
-          maxdamage = maxdamage + intelligenceamplifier;
+          lowdamage = lowdamage + intelligenceamplifier * level;
+          maxdamage = maxdamage + intelligenceamplifier * level;
           }*/
     }
 }
