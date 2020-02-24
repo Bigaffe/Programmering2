bf9a12bf88a6d0d44ef03dd686107ae344ed4412
Alfred Sjöholm <alfsjo0116@skynet.halmstad.se>
2020-02-24 09:41:04 +0100

    Server/klient

---
+++ natverk1/natverk1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace natverk1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+
+            byte[] message = Encoding.Unicode.GetBytes(textBox3.Text);
+
+            IPAddress serverIp = IPAddress.Parse( textBox1.Text);
+            int port = int.Parse(textBox2.Text);
+            IPEndPoint destination = new IPEndPoint(serverIp, port);
+
+            UdpClient klient = new UdpClient();
+            klient.Send(message, message.Length, destination);
+        }
+    }
+}
---
+++ natverk1server/natverk1server/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace natverk1server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btntagemot = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(146, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 100);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(234, 109);
+            this.textBox2.TabIndex = 2;
+            // 
+            // btntagemot
+            // 
+            this.btntagemot.Location = new System.Drawing.Point(13, 216);
+            this.btntagemot.Name = "btntagemot";
+            this.btntagemot.Size = new System.Drawing.Size(236, 23);
+            this.btntagemot.TabIndex = 3;
+            this.btntagemot.Text = "Tag emot";
+            this.btntagemot.UseVisualStyleBackColor = true;
+            this.btntagemot.Click += new System.EventHandler(this.Btntagemot_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 245);
+            this.Controls.Add(this.btntagemot);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btntagemot;
+    }
+}
+
