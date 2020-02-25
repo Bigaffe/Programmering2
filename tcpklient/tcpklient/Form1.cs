2ef6908b1bf254702509de4aceb087363bcaea7a
Alfred Sjöholm <alfsjo0116@skynet.halmstad.se>
2020-02-25 13:30:58 +0100

    Klient och server fungerar fint fint med varandra om man inte räknar med några buggar som slutar med en krash i programmet

---
+++ tcpklient/tcpklient/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace tcpklient
+{
+
+    public partial class Form1 : Form
+    {
+        TcpClient klient;
+        int port = 12345;
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            IPAddress adress = IPAddress.Parse(textBox1.Text);
+            klient = new TcpClient();
+            klient.NoDelay = true;
+            if (!klient.Connected)
+            {
+                klient.Connect(adress, port);
+            }
+            //try
+            //{
+
+
+                if (klient.Connected)
+                {
+                    byte[] utData = Encoding.Unicode.GetBytes(textBox2.Text);
+                    klient.GetStream().Write(utData, 0, utData.Length);
+                    klient.Close();
+                }
+            //}
+            /*catch
+            {
+                byte[] utData = Encoding.Unicode.GetBytes(textBox2.Text);
+                klient.GetStream().Write(utData, 0, utData.Length);
+                klient.Close();
+
+            }
+            */
+
+        }
+    }
+}
---
+++ tcpserver/tcpserver/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace tcpserver
+{
+    public partial class Form1 : Form
+    {
+        TcpListener lyssnare;
+        TcpClient klient;
+            int port = 12345;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            lyssnare = new TcpListener(IPAddress.Any, port);
+            lyssnare.Start();
+
+            klient = lyssnare.AcceptTcpClient();
+
+            byte[] inData = new byte[256];
+
+            int antalByte = klient.GetStream().Read(inData, 0, inData.Length);
+
+            textBox1.Text = Encoding.Unicode.GetString(inData, 0, antalByte);
+            klient.Close();
+            lyssnare.Stop();
+            
+        }
+    }
+}
